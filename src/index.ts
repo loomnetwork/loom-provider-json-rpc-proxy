a039fe83c7ad6faa9f7b98560803538ac539b6e0
Axel Henriot <axlht@hotmail.com>
2019-11-05 11:44:44 -0400

    Added rinkeby key generation and address mapping

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,20 @@
 import http from 'http'
-import { CryptoUtils, LoomProvider, Client, ClientEvent } from 'loom-js'
+import {
+  CryptoUtils,
+  LoomProvider,
+  Client,
+  ClientEvent,
+  LocalAddress,
+  Address,
+  Contracts,
+  NonceTxMiddleware,
+  SignedTxMiddleware
+} from 'loom-js'
 import { IEthRPCPayload } from 'loom-js/dist/loom-provider'
+import Web3 from 'web3'
+import { OfflineWeb3Signer } from 'loom-js/dist/solidity-helpers'
+
+const AddressMapper = Contracts.AddressMapper
 
 // Default chain id
 const chainId = process.env.CHAIN_ID || 'default'
@@ -12,9 +26,63 @@ const chainEndpoint = process.env.CHAIN_ENDPOINT || 'wss://plasma.dappchains.com
 const port = process.env.PORT || 8080
 
 // Initialize Client and LoomProvider
-const privateKey = CryptoUtils.generatePrivateKey()
-const client = new Client(chainId, `${chainEndpoint}/websocket`, `${chainEndpoint}/eth`)
-const loomProvider = new LoomProvider(client, privateKey)
+const privateKeyLoom = CryptoUtils.generatePrivateKey()
+const client = new Client(chainId, `${chainEndpoint}/websocket`, `${chainEndpoint}/queryws`)
+const loomPublicKey = CryptoUtils.publicKeyFromPrivateKey(privateKeyLoom)
+client.txMiddleware = [
+  new NonceTxMiddleware(loomPublicKey, client),
+  new SignedTxMiddleware(privateKeyLoom)
+]
+const loomProvider = new LoomProvider(client, privateKeyLoom)
+
+// create Rinkeby address
+
+const InfuraAPIkey = process.env.INFURA_API_KEY
+
+const privateKeyRinkeby = CryptoUtils.generatePrivateKey()
+const web3js = new Web3(`https://rinkeby.infura.io/v3/${InfuraAPIkey}`)
+const ownerAccount = web3js.eth.accounts.privateKeyToAccount('0x' + privateKeyRinkeby)
+web3js.eth.accounts.wallet.add(ownerAccount)
+
+const rinkebyAddress = ownerAccount.address
+
+// get Loom address
+
+const loomAddress = LocalAddress.fromPublicKey(loomPublicKey).toString()
+
+console.log(`Rinkeby address : ${rinkebyAddress}`)
+console.log(`Loom address : ${loomAddress}`)
+
+// mapping accounts
+
+const signer = new OfflineWeb3Signer(web3js, ownerAccount)
+
+async function mapAccounts(client, signer, ownerRinkebyAddress, ownerExtdevAddress) {
+  const ownerRinkebyAddr = Address.fromString(`eth:${ownerRinkebyAddress}`)
+  const ownerExtdevAddr = Address.fromString(`${client.chainId}:${ownerExtdevAddress}`)
+
+  let mapperContract
+  mapperContract = await AddressMapper.createAsync(client, ownerExtdevAddr)
+
+  try {
+    const mapping = await mapperContract.getMappingAsync(ownerExtdevAddr)
+    console.log(`${mapping.from.toString()} is already mapped to ${mapping.to.toString()}`)
+    return
+  } catch (err) {
+    // assume this means there is no mapping yet, need to fix loom-js not to throw in this case
+  }
+  console.log(`mapping ${ownerRinkebyAddr.toString()} to ${ownerExtdevAddr.toString()}`)
+
+  try {
+    await mapperContract.addIdentityMappingAsync(ownerExtdevAddr, ownerRinkebyAddr, signer)
+  } catch (err) {
+    console.log(err)
+  }
+
+  console.log(`Mapped ${ownerExtdevAddr} to ${ownerRinkebyAddr}`)
+}
+
+mapAccounts(client, signer, rinkebyAddress, loomAddress)
 
 // Used by Remix https://remix.ethereum.org
 loomProvider.addCustomMethod('net_listening', (payload: IEthRPCPayload) => true)
